fdc85656b8010aefaef31c28c382e4a61cbb2ecb
Douglas Obara <douglasobara97@gmail.com>
2022-06-18 18:57:02 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+
+  for (let i = 0; i < len / 2; i++) {
+    if (word[i] !== word[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode heres
 */
 
 /*
   Add written explanation of your solution here
+
+function isPalindrome(word) {}  // program to check if the string is palindrome or not
+ 
+const len = word.length; // find the length of a string
+
+for (let i = 0; i < len / 2; i++) {} // loop through half of the string
+
+      // check if first and last string are same
+        if (word[i] !== word[len - 1 - i]) {
+            return false;
+        }
+    
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +40,9 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
